ad44e35e3516d1ac13bbf27e2cc19ac00eace8ba
Kanishka Raajkumar <kanishkaraajkumar@Kanishkas-MacBook-Pro-2.local>
2017-11-21 10:03:55 -0500

    Removed a block of code that was causing a out of range crash when syncing an ordered to-many relationship

--- Source/Sync/NSManagedObject+Sync.swift
+++ Source/Sync/NSManagedObject+Sync.swift
@@ -276,25 +276,6 @@ extension NSManagedObject {
                             }
                         }
                     }
-
-                    if relationship.isOrdered {
-                        let objects: [NSManagedObject]
-                        if let safeLocalObjects = safeLocalObjects {
-                            objects = safeLocalObjects
-                        } else {
-                            objects = try managedObjectContext.fetch(request) as? [NSManagedObject] ?? [NSManagedObject]()
-                        }
-                        for safeObject in objects {
-                            let currentID = safeObject.value(forKey: safeObject.entity.sync_localPrimaryKey())!
-                            let remoteIndex = remoteItems.index(of: currentID)
-                            let relatedObjects = self.mutableOrderedSetValue(forKey: relationship.name)
-
-                            let currentIndex = relatedObjects.index(of: safeObject)
-                            if currentIndex != remoteIndex && currentIndex != NSNotFound {
-                                relatedObjects.moveObjects(at: IndexSet(integer: currentIndex), to: remoteIndex)
-                            }
-                        }
-                    }
                 }
             }
 
